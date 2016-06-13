0a2dba27db1f9af8af812ce53e05388751386429
Joseph <giuseppe.turelli@gmail.com>
2016-06-13 23:17:46 +0200

    Tried testing envelope functions but not working yet

--- helperCryptoFunc.cpp
+++ helperCryptoFunc.cpp
@@ -9,7 +9,7 @@
 #include <openssl/err.h>
 
 static const int bufferLength = 2048;
-static const int keyLength = 32;
+static const int keyLength = 16;
 static const int ivLength = 16;
 
 void generateRandomBuffer(unsigned char ioRandBuffer[], int size) {
@@ -26,7 +26,7 @@ void errorHandle() {
 
 struct Data {
     unsigned char data[bufferLength];
-    unsigned int length = 0;
+    long unsigned int length = 0;
     long unsigned int availableSpace = bufferLength;
 };
 
@@ -42,7 +42,7 @@ void envelope_seal(EVP_PKEY** publicKey, const Data& toEncrypt, Data& oEncrypted
 
     if(!(ctx = EVP_CIPHER_CTX_new()))
         errorHandle();
-    
+$
     if (1 != EVP_SealInit(ctx, EVP_aes_128_cbc(), (unsigned char **) &oAESData.key, &oAESData.length, oAESData.initVector, publicKey, 1))
         errorHandle();
 
@@ -84,7 +84,7 @@ void encrypt(const AESData& iAESData, const Data& toEncrypt, Data& oEncryptedDat
 
     //START: Message Encryption operation
     int partialLength = 0;
-    if (1 != EVP_EncryptInit_ex(cipherCtx, EVP_aes_256_cbc(), NULL, iAESData.key, iAESData.initVector))
+    if (1 != EVP_EncryptInit_ex(cipherCtx, EVP_aes_128_cbc(), NULL, iAESData.key, iAESData.initVector))
         errorHandle();
 
     if (1 != EVP_EncryptUpdate(cipherCtx, oEncryptedData.data, &partialLength, toEncrypt.data,
@@ -107,7 +107,7 @@ void decrypt(const AESData& iAESData, const Data& toDecrypt, Data& oDecryptedDat
     //START: Decryption operation
     int partialDataLength = 0;
 
-    if (1 != EVP_DecryptInit_ex(cipherCtx, EVP_aes_256_cbc(), NULL, iAESData.key, iAESData.initVector))
+    if (1 != EVP_DecryptInit_ex(cipherCtx, EVP_aes_128_cbc(), NULL, iAESData.key, iAESData.initVector))
         errorHandle();
 
     if (1 != EVP_DecryptUpdate(cipherCtx, oDecryptedData.data, &partialDataLength, toDecrypt.data, toDecrypt.length))
@@ -131,10 +131,10 @@ void sign(EVP_PKEY* privateKey, const Data& toSign, Data& oSignatureData) {
     if (1 != EVP_DigestSignUpdate(digestSignCtx, toSign.data, toSign.length))
         errorHandle();
 
-    if (1 != EVP_DigestSignFinal(digestSignCtx, NULL, &oSignatureData.length))
+    if (1 != EVP_DigestSignFinal(digestSignCtx, NULL, (size_t*) &oSignatureData.length))
         errorHandle();
 
-    if (1 != EVP_DigestSignFinal(digestSignCtx, oSignatureData.data, &oSignatureData.length))
+    if (1 != EVP_DigestSignFinal(digestSignCtx, oSignatureData.data, (size_t*) &oSignatureData.length))
         errorHandle();
     EVP_MD_CTX_cleanup(digestSignCtx);
     //END: Message Signing operation
@@ -158,11 +158,11 @@ bool verify(EVP_PKEY* publicKey, const Data& signedData, const Data& signatureDa
 
 int main(int argc, char* argv[]) {
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-    std::chrono::duration<double,std::micro> encryptionMicro, signingMicro, verifyingMicro, decryptionMicro; 
+    std::chrono::duration<double,std::micro> encryptionMicro, signingMicro, verifyingMicro, decryptionMicro;
     int ret = -1;
     unsigned char key[keyLength];
     unsigned char initVector[ivLength];
-    
+
     //Generating key and IV
     generateRandomBuffer(key, sizeof(key));
     generateRandomBuffer(initVector, sizeof(initVector));
@@ -173,18 +173,6 @@ int main(int argc, char* argv[]) {
 
     //std::string toEncrypt = "u8bZgY4IB0CHtAxNTLpa8oCWji8kvAqFx07Mb3sptkBC9RPS3kOe3w4xVFvv77Go01LG2yXzk300yTTJxNNRzv5BDt2LeWcbqhKgIJli1gjlpgy2yeueLaTrkOBMPKIWq1GNyv3E3k5u8kkQUzDumrUUvu6XZvBstOlWKcni2k3lHD382yaDhwvvPau8Acz7Uucaeg1hTr3G0VB2ESSVssAwzbGgS5OUfA24U2ifSOe4IncxWB8WJF9NXbytoM7gSbF2M20iPRUhtqnTDi4oQxDEUUiySCjKRh2kUNQ6Qv4tAfiMbtei6fOrxF6Ivb6oCCY0E2m2OuIOTPVrvVt0s8x2u6oiElyIwjG7oa70TvLEaFRs6rRRNznHf7WyvTeCn0xCPQwYCWXHzaAnDbNIoQv6XlWkNwry1AZRkESvXg8zqkmCYgY8STBZC1nk5El8yGCFUvSnUM4tDgMUh0cUQDiwcRjzHM5b4ZnvTLcLrZ5g5J8PrHe4zPxquj0BCHD3ghUb0oxSqLALTI0qmfGtXuQ9yiAVL8Pq4lY7aSlvfcP2z5V9xTPOsgb5p6hNEGrj8BfswkXrva5pZ6YmD0nvv6GJhDLC0lbW20XWmVr9RR1XkHXUTmZx7DGvrKoG8SOJnKuYWEoHstqNr11LvowKPuKNEzKN4Octy8kH9yFu3Y007qz5cINSXuJajuuUHcVnK1z45cUikeSwbffBVr2tugmEsMbgZKuNTMgzpu2juK0AQ7Y0N4CNgaXTv96vR0Kr2iBeMGnGlBQ8tSjf6cizPbGQrLkRs96VR8Xp6r3b0i08ywapEAPv38eQHWvu093JZcUTpmp13VzeJK9mvphaYWQmaFJU9i8qkRrI5crFItCh0Z4BSEkvlJwwFMhtQv78AzDjWzfbxDaVS1XSk2p5REDS3PmGx9vQts7W90rJuSxsEiLbNS4hNjKx1YeuvCinoTkhwcAEqx4gpBJT7ucRaNHooOK7eEPM03WzSUne2efWfK6MQrNhXD78N9elDYww";
     std::string toEncrypt = "AllTheseMomentsWillBeLostInTimeLikeTearsInRainxAllTheseMomentsWillBeLostInTimeLikeTearsInRainxAllTheseMomentsWillBeLostInTimeLikeTearsInRainxAllTheseMomentsWillBeLostInTimeLikeTearsInRainx";
-
-    Data aToEncrypt;
-    memcpy(aToEncrypt.data, toEncrypt.c_str(), toEncrypt.length());
-    aToEncrypt.length = toEncrypt.length();
-    
-    //Encrypting
-    Data aEncryptedData;
-    start = std::chrono::high_resolution_clock::now();
-    encrypt(aAESData, aToEncrypt, aEncryptedData);
-    end = std::chrono::system_clock::now(); 
-    encryptionMicro = end - start;
-
     //Getting RSA Private key
     FILE* fp;
     EVP_PKEY* privateKey;
@@ -198,14 +186,6 @@ int main(int argc, char* argv[]) {
     } else {
         std::cout << "Private RSA key missing, exiting!" << std::endl;
     }
-
-    //Signing
-    Data aSignatureData;
-    start = std::chrono::high_resolution_clock::now();
-    sign(privateKey, aEncryptedData, aSignatureData);
-    end = std::chrono::system_clock::now(); 
-    signingMicro = end - start;
-
     //Getting RSA Public key
     EVP_PKEY* publicKey;
 
@@ -221,10 +201,36 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    Data aToEncrypt;
+    memcpy(aToEncrypt.data, toEncrypt.c_str(), toEncrypt.length());
+    aToEncrypt.length = toEncrypt.length();
+
+
+    /*
+    //Encrypting
+    Data aEncryptedData;
+    AESData aAESData;
+    envelope_seal(&privateKey, aToEncrypt, aEncryptedData, aAESData);
+    */
+    //Encrypting
+    Data aEncryptedData;
+    start = std::chrono::high_resolution_clock::now();
+    encrypt(aAESData, aToEncrypt, aEncryptedData);
+    end = std::chrono::system_clock::now();
+    encryptionMicro = end - start;
+
+    //Signing
+    Data aSignatureData;
+    start = std::chrono::high_resolution_clock::now();
+    sign(privateKey, aEncryptedData, aSignatureData);
+    end = std::chrono::system_clock::now();
+    signingMicro = end - start;
+
+
     //Verifying
     start = std::chrono::high_resolution_clock::now();
     bool verified= verify(publicKey, aEncryptedData, aSignatureData);
-    end = std::chrono::system_clock::now(); 
+    end = std::chrono::system_clock::now();
     verifyingMicro = end - start;
 
 
@@ -233,11 +239,17 @@ int main(int argc, char* argv[]) {
     else
         std::cout << "[NOT] Signature *not* VERIFIED! [NOT]" << std::endl;
 
+    /*
+    //Decryption
+    Data aDecryptedData;
+    envelope_open(publicKey, aEncryptedData, aDecryptedData, aAESData);
+    */
+
     //Decryption
     Data aDecryptedData;
     start = std::chrono::high_resolution_clock::now();
     decrypt(aAESData, aEncryptedData, aDecryptedData);
-    end = std::chrono::system_clock::now(); 
+    end = std::chrono::system_clock::now();
     decryptionMicro = end - start;
 
     std::string decryptedDataStr = std::string((const char*)aDecryptedData.data).substr(0, aDecryptedData.length);
