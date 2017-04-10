27e2f8c59d0ef1e6fb3da6a11a170db784cd3ac5
Joseph <giuseppe.turelli@gmail.com>
2017-04-10 15:19:30 +0200

    with the previous commit

--- CryptoStructures.h
+++ CryptoStructures.h
@@ -1,3 +1,6 @@
+#ifndef CRYPTO_STRUCTURES_H
+#define CRYPTO_STRUCTURES_H
+
 #include <vector>
 #include <string.h>
 #include <iostream>
@@ -10,15 +13,15 @@ class Data {
     private:
         std::vector<unsigned char> data_;
     public:
-        Data() {}
-        Data(int size) { data_.resize(size); }
+        Data();
+        Data(int size);
 
-        unsigned char* dataPtr() { return &data_[0]; }
-        const unsigned char* dataPtr() const { return &data_[0]; }
-        void resize(int size) { data_.resize(size); }
-        int size(){ return data_.size(); }
-        const int size() const { return data_.size(); }
-        bool equal(const Data& toCompare) { return std::equal(data_.begin(), data_.end(), toCompare.data_.begin()); }
+        unsigned char* dataPtr();
+        const unsigned char* dataPtr() const;
+        void resize(int size);
+        int size();
+        const int size() const;
+        bool equal(const Data& toCompare);
 
 };
 
@@ -27,12 +30,6 @@ class EncryptedData {
         Data encryptedData;
         Data initVector;
 };
-void errorHandle() {
-    char error[1024];
-    ERR_load_crypto_strings();
-    ERR_error_string_n(ERR_get_error(), error, 1024);
-    std::cout << "Error value: " << error << std::endl;
-    throw 1;
-}
+void errorHandle();
 }//namespace CryptoUtils
-
+#endif //CRYPTO_STRUCTURES_H
